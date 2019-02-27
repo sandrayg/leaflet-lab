5efe607ef215f69b04145244bb2a2b0c66bfa588
sandrayg <46978792+sandrayg@users.noreply.github.com>
2019-02-27 15:38:37 -0600

    Module 5A assignment completed

--- js/main.js
+++ js/main.js
@@ -16,31 +16,60 @@ function createMap(){
     //call getData function
     getData(map);
 };
+//calculate the radius of each proportional symbol
+function calcRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 100;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
 
-// retrieve the MegaCities data and place it on the map as circle markers
+    return radius;
+};
+
+function pointToLayer(feature, latlng){
+  var attribute = "yr2017";
+  var year = attribute.slice(2);
+  var geojsonMarkerOptions = { //set the markers style
+      fillColor: "#ff7800",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+  };
+  var attValue = Number(feature.properties[attribute]); //determine which attribute to be represented by propotional createPropSymbols
+  var attName = Number(feature.properties['Indicator']);
+  markerOptions.radius = calcRadius(attValue); //assign the radius value in the marker option
+  var layer = L.circleMarker(latlng, markerOptions);
+  //build popup content string
+    var popupContent = "<p><b>Country:</b> " + feature.properties.CountryName + "</p><p><b> " + attName+' in  '+year + ":</b> " + feature.properties[attribute] + "</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent);
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data, map){
+      //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: pointToLayer
+    }).addTo(map);
+};
+
+
+//Step 2: Import GeoJSON data
 function getData(map){
-    //load my data, which contain 153 point features (countries)
+    //load the data
     $.ajax("data/lab1_data.geojson", {
-            dataType: "json",
-            success: function(response){
-                //create marker options
-                var geojsonMarkerOptions = {
-                    radius: 8,
-                    fillColor: "#ff7800", //orange color
-                    color: "#000",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                };
-
-                //create a Leaflet GeoJSON layer and add it to the map
-                L.geoJson(response, {
-                    pointToLayer: function (feature, latlng){
-                        return L.circleMarker(latlng, geojsonMarkerOptions);
-                    }
-                }).addTo(map);
-            }
-        });
+        dataType: "json",
+        success: function(response){
+            //call function to create proportional symbols
+            createPropSymbols(response, map);
+        }
+    });
 };
 
 $(document).ready(createMap);
